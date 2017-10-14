a32291df7ed14f377579580cc36d98787c4e6277
toryu1121 <y28bdgw9@gmail.com>
2017-10-14 13:39:26 +0000

    puyo2

--- app/controllers/puyo_controller.rb
+++ app/controllers/puyo_controller.rb
@@ -1,9 +1,23 @@
 class PuyoController < ApplicationController
   def index
+    @test10d_list = Puyo.order("created_at DESC").limit(10)
   end
 
   def create
+    @test10d_list = Puyo.order("created_at DESC").limit(5)
+    @test10d = Puyo.new(params.permit(:field))
+    
+    p "#####okkkk#######"
+    Puyo.new do |i|
+      i.field = "momoken"
+      i.save
+    end
+    
+    respond_to do |format|
+        format.js
+    end
   end
+
 
   def update
   end
